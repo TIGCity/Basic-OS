ba79cd5e6c1161e406b463c3b86b0c3207221233
Brenden2008 <42918416+Brenden2008@users.noreply.github.com>
2020-12-19 16:39:17 -0600

    Create sms.lua

---
+++ apps/sms.lua
@@ -0,0 +1,10 @@
+local cin = fs.open("/settings/cin.txt", "r")
+local thisnumber = cin.readAll()
+print("Please type your name: ")
+name = read()
+print("What is the number you are texting?: ")
+number2 = read()
+print("NOTE: Currently the person you are texting will have to open the messanger at the same time as you to see your message.")
+sleep(5)
+number = "tigcity-" + number2 + thisnumber
+shell.run("enchatapi", name, number)
