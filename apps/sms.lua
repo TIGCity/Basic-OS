6443277bd50e0e5c6e739270d61af9e26379807a
Brenden2008 <42918416+Brenden2008@users.noreply.github.com>
2020-12-19 17:19:48 -0600

    Update sms.lua

--- apps/sms.lua
+++ apps/sms.lua
@@ -1,10 +1,6 @@
-local cin = fs.open("/settings/cin.txt", "r")
-local thisnumber = cin.readAll()
-print("Please type your name: ")
+print("Type the chat id to connect to: ")
+chatId2 = read()
+print("Type your name: ")
 name = read()
-print("What is the number you are texting?: ")
-number2 = read()
-print("NOTE: Currently the person you are texting will have to open the messanger at the same time as you to see your message.")
-sleep(5)
-number = "tigcity-"..name..""..number2..""
-shell.run("enchatapi", name, number)
+chatId = "tigcity-"..chatId2..""
+shell.run("enchatapi", name, chatId)
